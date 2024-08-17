e8f90f3a9bf44a803f4e9b46f202b1a04757fc6c
ScarCruft (Reaper) <175374158+APT37@users.noreply.github.com>
2024-08-17 04:38:33 +0200

    Update MonsterDef.java
    add/remove white space 2/2

--- doorsofdoom/src/main/java/com/github/msx80/doorsofdoom/model/MonsterDef.java
+++ doorsofdoom/src/main/java/com/github/msx80/doorsofdoom/model/MonsterDef.java
@@ -186,94 +186,157 @@ public enum MonsterDef {
 		range(10, 20),
 		range(39, 65),
 		range(20, 22),
-		Loot.of(5, Item.Rejuvenant, range(2,4)),
-		Loot.of(3, Item.BagOfGold, range(2,3))
+		Loot.of(5, Item.Rejuvenant, range(2, 4)),
+		Loot.of(3, Item.BagOfGold, range(2, 3))
 	),
-
-	 EVILEYE("EVIL EYE",-26,range(20, 27),range(8, 13),
-				range(40,55), range(15,18),				
-				Loot.of(3, Item.Stick, range(3,5)),
-				Loot.of(1, Item.Clover, range(2,3))),
-	 
-	 KITTEN("LOST KITTEN",-28,range(2, 3),range(5, 6),
-				range(40,41), range(10,15),				
-				Loot.of(3, Item.BagOfGold, range(3,4))),
-			 
-	 GOLEM("GOLEM",-8,range(50,60),range(15,20),
-				range(40,1000), range(10,20),
-				Loot.of(3, Item.Rock, range(5, 8))),
-	 
-	 DARKKNIGHT("DARK KNIGHT",-18,range(50,80),range(20,30),
-				range(43,79), range(8,20),
-				Loot.of(1, Item.Shield, range(1,1)),
-				Loot.of(1, Item.Armour, range(1,1)),
-				Loot.of(1, Item.Sword, range(1,1))),
-	 
-	 JESTER("JESTER",-31,range(15,16),range(0,25),
-				range(40,69), range(3,8),
-				Loot.of(2, Item.Rock, range(1,3)),
-				Loot.of(2, Item.Clover, range(1,3)),
-				Loot.of(2, Item.MintLeaf, range(2,3)),
-				Loot.of(2, Item.Rejuvenant, range(1,2)),
-				Loot.of(2, Item.Pants, range(1,1)),
-				Loot.of(2, Item.Tomato, range(2,3))),
-
-	 KEYMASTER("KEY MASTER",-25,range(25, 35),range(10, 15),
-				range(45,1000), range(15,20),
-				Loot.of(1, Item.Key, range(3,5))),
-	 
-	 SUCCUBUS("SUCCUBUS",-17,range(40,60),range(20, 25),
-		range(49,89), range(10,12),
-		Loot.of(1, Item.Helm, range(1,1)),
-		Loot.of(5, Item.Gold, range(10,20))),
-	 
-	 SPRITES("SPRITES",-33,range(40, 60),range(5, 40),
-		range(55,85), range(30,50),
-		Loot.of(1, Item.Ectoplasm, range(4,8)),
-		Loot.of(2, Item.Sprite, range(2,3)),
-		Loot.of(1, Item.Rejuvenant, range(2,4))),
-	 
-	 TARDIGRADE("TARDIGRADE",-13,range(80,100),range(15,25),
-		range(59,1000), range(3,6),
-		Loot.of(2, Item.Spinach, range(1,3)),
-		Loot.of(2, Item.Leather, range(6,8))),
-
-	 MAGE("MAGE",-9,range(45,55),range(30,60),
-		range(55,1000), range(15,30),
-		Loot.of(5, Item.BigPotion, range(2,2)),
-		Loot.of(2, Item.Elixir, range(1,1)),
-		Loot.of(3, Item.Scroll, range(2,4))),	 	 
-	
-	 BLOB("BLOB",-35,range(35, 45),range(10, 15),
-		range(60,90), range(4,20),
-		Loot.of(1, Item.Gelatin, range(2,3)),
-		Loot.of(1, Item.Slime, range(4,6))),
-
-	 VAMPIRE("VAMPIRE",-32,range(40, 70),range(20, 30),
-		range(65,1000), range(30,50),
-		Loot.of(1, Item.Sword, range(1,1)),
-		Loot.of(1, Item.Fang, range(2,2)),
-		Loot.of(1, Item.Blood, range(3,6))),
-		
-	 DEMON("DEMON",-19,range(50, 100),range(30, 50),
-		range(75,1000), range(30,50),
-		Loot.of(3, Item.Key, range(2,3)),
-		Loot.of(1, Item.Diamond, range(1,1))),
-
-	 DRAGON("DRAGON",-30,range(80, 150),range(20, 40),
-		range(70,1000), range(1,50),
-		Loot.of(1, Item.Fang, range(2,2)),
-		Loot.of(1, Item.BagOfGold, range(2,3)),
-		Loot.of(1, Item.Blood, range(3,6))),
-		
-	 
-	 DEVIL("DEVIL",-24,range(200, 200),range(20, 300),
-				range(100,1000), range(30,50),
-				Loot.of(1, Item.Crown, range(1,1))),
-	 ANCIENT("ANCIENT",-36,range(400, 400),range(100, 1000),
-				range(200,1000), range(30,50),
-				Loot.of(1, Item.Wisdom, range(1,1))),
-	
+	EVILEYE(
+		"EVIL EYE", -26,
+		range(20, 27),
+		range(8, 13),
+		range(40, 55),
+		range(15, 18),
+		Loot.of(3, Item.Stick, range(3, 5)),
+		Loot.of(1, Item.Clover, range(2, 3))
+	),
+	KITTEN(
+		"LOST KITTEN", -28,
+		range(2, 3),
+		range(5, 6),
+		range(40, 41),
+		range(10, 15),
+		Loot.of(3, Item.BagOfGold, range(3, 4))
+	),
+	GOLEM(
+		"GOLEM", -8,
+		range(50, 60),
+		range(15, 20),
+		range(40, 1000),
+		range(10, 20),
+		Loot.of(3, Item.Rock, range(5, 8))
+	),
+	DARKKNIGHT(
+		"DARK KNIGHT", -18,
+		range(50, 80),
+		range(20, 30),
+		range(43, 79),
+		range(8, 20),
+		Loot.of(1, Item.Shield, range(1, 1)),
+		Loot.of(1, Item.Armour, range(1, 1)),
+		Loot.of(1, Item.Sword, range(1, 1))
+	),
+	JESTER(
+		"JESTER", -31,
+		range(15, 16),
+		range(0, 25),
+		range(40, 69),
+		range(3, 8),
+		Loot.of(2, Item.Rock, range(1, 3)),
+		Loot.of(2, Item.Clover, range(1, 3)),
+		Loot.of(2, Item.MintLeaf, range(2, 3)),
+		Loot.of(2, Item.Rejuvenant, range(1, 2)),
+		Loot.of(2, Item.Pants, range(1, 1)),
+		Loot.of(2, Item.Tomato, range(2, 3))
+	),
+	KEYMASTER(
+		"KEY MASTER", -25,
+		range(25, 35),
+		range(10, 15),
+		range(45, 1000),
+		range(15, 20),
+		Loot.of(1, Item.Key, range(3, 5))
+	),
+	SUCCUBUS(
+		"SUCCUBUS", -17,
+		range(40, 60),
+		range(20, 25),
+		range(49, 89),
+		range(10, 12),
+		Loot.of(1, Item.Helm, range(1, 1)),
+		Loot.of(5, Item.Gold, range(10, 20))
+	),
+	SPRITES(
+		"SPRITES", -33,
+		range(40, 60),
+		range(5, 40),
+		range(55, 85),
+		range(30, 50),
+		Loot.of(1, Item.Ectoplasm, range(4, 8)),
+		Loot.of(2, Item.Sprite, range(2, 3)),
+		Loot.of(1, Item.Rejuvenant, range(2, 4))
+	),
+	TARDIGRADE(
+		"TARDIGRADE", -13,
+		range(80, 100),
+		range(15, 25),
+		range(59, 1000),
+		range(3, 6),
+		Loot.of(2, Item.Spinach, range(1, 3)),
+		Loot.of(2, Item.Leather, range(6, 8))
+	),
+	MAGE(
+		"MAGE", -9,
+		range(45, 55),
+		range(30, 60),
+		range(55, 1000),
+		range(15, 30),
+		Loot.of(5, Item.BigPotion, range(2, 2)),
+		Loot.of(2, Item.Elixir, range(1, 1)),
+		Loot.of(3, Item.Scroll, range(2, 4))
+	),
+	BLOB(
+		"BLOB", -35,
+		range(35, 45),
+		range(10, 15),
+		range(60, 90),
+		range(4, 20),
+		Loot.of(1, Item.Gelatin, range(2, 3)),
+		Loot.of(1, Item.Slime, range(4, 6))
+	),
+	VAMPIRE(
+		"VAMPIRE", -32,
+		range(40, 70),
+		range(20, 30),
+		range(65, 1000),
+		range(30, 50),
+		Loot.of(1, Item.Sword, range(1, 1)),
+		Loot.of(1, Item.Fang, range(2, 2)),
+		Loot.of(1, Item.Blood, range(3, 6))
+	),
+	DEMON(
+		"DEMON", -19,
+		range(50, 100),
+		range(30, 50),
+		range(75, 1000),
+		range(30, 50),
+		Loot.of(3, Item.Key, range(2, 3)),
+		Loot.of(1, Item.Diamond, range(1, 1))
+	),
+	DRAGON(
+		"DRAGON", -30,
+		range(80, 150),
+		range(20, 40),
+		range(70, 1000),
+		range(1, 50),
+		Loot.of(1, Item.Fang, range(2, 2)),
+		Loot.of(1, Item.BagOfGold, range(2, 3)),
+		Loot.of(1, Item.Blood, range(3, 6))
+	),
+	DEVIL(
+		"DEVIL", -24,
+		range(200, 200),
+		range(20, 300),
+		range(100, 1000),
+		range(30, 50),
+		Loot.of(1, Item.Crown, range(1, 1))
+	),
+	ANCIENT(
+		"ANCIENT", -36,
+		range(400, 400),
+		range(100, 1000),
+		range(200, 1000),
+		range(30, 50),
+		Loot.of(1, Item.Wisdom, range(1, 1))
+	),
 	;
 	
 	public String name;
